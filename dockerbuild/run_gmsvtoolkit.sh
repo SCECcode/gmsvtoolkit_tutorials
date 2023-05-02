a7e00d0be6fd5b72f493a99dbbb2b2f9e3844d26
Fabio Silva <fsilva@usc.edu>
2023-05-02 08:05:13 -0700

    Added docker building scripts

---
+++ dockerbuild/run_gmsvtoolkit.sh
@@ -0,0 +1,3 @@
+# -rm removes container on exit
+# -it indicates inteactive + --tty so that users are at an interactive tty command line when the container starts
+docker run -p 8888:8888 --rm -i -t --mount type=bind,source="$(pwd)"/target,destination=/home/scecuser/target gmsvtoolkit_jupyter:latest
