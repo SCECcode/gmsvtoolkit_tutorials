01ce345f3c585040611f537b4602f40b6dbe6a4a
Fabio Silva <fsilva@usc.edu>
2023-05-04 18:58:27 -0700

    Updated run script to create target folder automatically if it is not there

--- dockerbuild/run_gmsvtoolkit.sh
+++ dockerbuild/run_gmsvtoolkit.sh
@@ -1,3 +1,5 @@
 # -rm removes container on exit
 # -it indicates inteactive + --tty so that users are at an interactive tty command line when the container starts
-docker run -p 8888:8888 --rm -i -t --mount type=bind,source="$(pwd)"/target,destination=/home/scecuser/target gmsvtoolkit_jupyter:latest
+mkdir -p target
+
+docker run -p 8888:8888 --rm -i -t --mount type=bind,source="$(pwd)"/target,destination=/home/scecuser/target sceccode/gmsvtoolkit_jupyter:latest
