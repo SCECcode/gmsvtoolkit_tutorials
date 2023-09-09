5158552c0b3f53d468bf618c8b08d8bce40a3f59
Philip Maechling <pjmaechling@users.noreply.github.com>
2023-09-09 11:39:33 -0700

    Changed from notebook to lab interface

--- dockerbuild/run_gmsvtoolkit.sh
+++ dockerbuild/run_gmsvtoolkit.sh
@@ -2,4 +2,5 @@
 # -it indicates inteactive + --tty so that users are at an interactive tty command line when the container starts
 mkdir -p target
 
-docker run -p 8888:8888 sceccode/gmsvtoolkit_jupyter:latest
+#docker run -p 8888:8888 sceccode/gmsvtoolkit_jupyter:latest
+docker run -p 8888:8888 gmsvtoolkit_jupyter:latest
